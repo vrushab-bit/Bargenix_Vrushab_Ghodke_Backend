a78cb0673ed28a8bcdaa21dfdc85218541586310
Vrushab <vrushab.ghodke@gmail.com>
2024-11-24 23:19:47 +0530

    Rate Limiting Functionality.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express, { urlencoded, json, Request, Response } from 'express'
 import AuthRoutes from './routes/AuthRoutes'
 import CouponRoutes from './routes/CouponRoutes'
+import { limiter } from './services/rateLimit'
 
 export default function createServer() {
   const app = express()
-
+  app.use(limiter)
   app.use(urlencoded({ extended: true }))
   app.use(json())
   app.use('/api/auth', AuthRoutes)
   app.use('/api/coupon', CouponRoutes)
 
   app.get('/health', async (req: Request, res: Response) => {
-    res.status(200).json({ message: 'OK' })
+    const healthStatus = {
+      uptime: process.uptime(),
+      message: 'OK',
+      timestamp: Date.now()
+    }
+    res.status(200).send(healthStatus)
   })
 
   return app
